8d6b261f08167652871b503935d19101bf449b6b
Aaron Robb <airrobb@gmail.com>
2016-04-01 16:01:27 -0400

    lines bruh

--- src/index.js
+++ src/index.js
@@ -1,10 +1,61 @@
 import d3 from 'd3'
 import request from 'superagent'
 
-const w = 300
-const h = 150
+const w = 700
+const h = 300
+//
+// const monthlySales = [
+//   {"month": 10, "sales": 200},
+//   {"month": 20, "sales": 140},
+//   {"month": 30, "sales": 200},
+//   {"month": 40, "sales": 130},
+//   {"month": 50, "sales": 125},
+//   {"month": 60, "sales": 220},
+//   {"month": 70, "sales": 90},
+//   {"month": 80, "sales": 60},
+//   {"month": 90, "sales": 130},
+//   {"month": 100, "sales": 70}
+// ]
+//
+// const lineFun = d3.svg.line()
+//                   .x(d => d.month*5)
+//                   .y(d => h - d.sales)
+//                   .interpolate('basis')
+//
+// const svg = d3.select('body').append('svg').attr({
+//   width: w, height:h
+// })
+// const viz = svg.append('path')
+//               .attr({
+//                 d: lineFun(monthlySales),
+//                 "stroke": "purple",
+//                 "stroke-width": 2,
+//                 "fill": "none"
+//               })
+//
+//
+// const labels = svg.selectAll('text')
+//                 .data(monthlySales)
+//                 .enter()
+//                 .append('text')
+//                 .text(d => d.sales)
+//                 .attr({
+//                   x: d => d.month * 5,
+//                   y: d => h - d.sales,
+//                   'font-size': '12px',
+//                   'font-family': 'sans-serif',
+//                   'fill': '#666666',
+//                   'text-anchor': 'end',
+//                   'dy': '.35em',
+//                   'font-weight': (d, i) => {
+//                     if (i === 0 || i === (monthlySales.length-1)) {
+//                       return 'bold'
+//                     } else {
+//                       return 'normal'
+//                     }
+//                   }
+//                 })
 
-const svg = d3.select('body').append('svg')
 // const padding = 2
 // const dataSet = [5, 10, 14, 20, 25, 2, 23, 12, 7]
 // const svg = d3.select('body').append('svg')
