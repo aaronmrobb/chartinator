0d26f171aa275001d3a95e8c59f01728044d52ee
Aaron Robb <airrobb@gmail.com>
2016-03-31 21:56:12 -0400

    starting line functionality

--- src/index.js
+++ src/index.js
@@ -2,39 +2,43 @@ import d3 from 'd3'
 import request from 'superagent'
 
 const w = 300
-const h = 100
-const padding = 2
-const dataSet = [5, 10, 14, 20, 25]
-const svg = d3.select('body').append('svg')
-                .attr('width', w)
-                .attr('height', h)
-
-function colorPicker(v) {
-  if (v <= 20) {
-    return '#666666'
-  } else {
-    return '#FF0033'
-  }
-}
-
-svg.selectAll('rect')
-  .data(dataSet)
-  .enter()
-  .append('rect')
-    .attr({x: (d, i) =>  i * (w / dataSet.length),
-          y: d => h - (d * 4),
-          width: w / dataSet.length - padding,
-          height: d => d * 4,
-          fill: d => colorPicker(d)
-        })
-
-    // .attr('y',
-    // .attr('width', w / dataSet.length - padding)
-    // .attr('height', function(d) {
-    //   return d * 4
-    // })
-    // .attr('fill', function(d) {
-    //   return '
-    // })
-
-//Multivalue Map
+const h = 150
+// const padding = 2
+// const dataSet = [5, 10, 14, 20, 25, 2, 23, 12, 7]
+// const svg = d3.select('body').append('svg')
+//                 .attr('width', w)
+//                 .attr('height', h)
+//
+// function colorPicker(v) {
+//   if (v <= 20) {
+//     return '#666666'
+//   } else {
+//     return '#FF0033'
+//   }
+// }
+//
+// svg.selectAll('rect')
+//   .data(dataSet)
+//   .enter()
+//   .append('rect')
+//     .attr({x: (d, i) =>  i * (w / dataSet.length),
+//           y: d => h - (d * 4),
+//           width: w / dataSet.length - padding,
+//           height: d => d * 4,
+//           fill: d => colorPicker(d)
+//         })
+//
+// svg.selectAll('text')
+//     .data(dataSet)
+//     .enter()
+//     .append('text')
+//     .text(d => d)
+//     .attr({
+//       "text-anchor": "middle",
+//       x: (d, i) => i * (w / dataSet.length) + (w / dataSet.length - padding)  / 2,
+//       y: d => h - (d * 4) + 14,
+//       fill: 'white',
+//       'font-family': 'sans-serif'
+//     })
+//
+//
